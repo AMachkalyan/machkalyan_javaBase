d89789ad06dff475a8c15fe5593ddfbed0444ddc
OlgaSedysheva <145748609+OlgaSedysheva@users.noreply.github.com>
2023-12-26 15:25:45 +0300

    Update twoNumbers.java
    убираю пробелы, не оставляй отступы ненужные

--- src/IsBigger/twoNumbers.java
+++ src/IsBigger/twoNumbers.java
@@ -13,8 +13,7 @@ public class twoNumbers {
         scanner.close();
         scanner.close();
     }
-
-
+    
     public static void isBigger(double num1, double num2) {
         if (num1 > num2)
             System.out.println(num1 + " is bigger");
